8254924364bd67062e5fcfc95827717ff26f03fa
BautistaLignini <jlignini@udesa.edu.ar>
2024-11-24 20:03:01 -0300

    search

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import NewPost from "../screens/NewPost"
 import Profile from '../screens/Profile';
+import Search from "../screens/Search";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +13,8 @@ export default function HomeMenu() {
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="New Post" component={NewPost} />
       <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Search" component={Search} />
+
     </Tab.Navigator>
   );
 }
---
+++ src/screens/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native-web';
+import SearchForm from '../components/SearchForm';
+
+
+export default function NewPost(props){
+    return(
+        <View>
+            <SearchForm />
+        </View>
+    )
+}
